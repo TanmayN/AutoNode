49bb1eee08c390082ed9a77e5ffb8237096095c9
TanmayN <nope@nope.nope>
2014-05-25 21:07:17 -0700

    Functions updated

--- includes/functions.php
+++ includes/functions.php
@@ -1,9 +1,7 @@
 <?php
 	include('config.php');
-	include('mysql.class.php');
 
-	$mysql = new MySQL;
-	$mysql->connect($db['host'], $db['username'], $db['password'], $db['database']);
+	$mysql = mysqli_connect($db['host'], $db['username'], $db['password'], $db['database']) or die("Database connection error: " . mysqli_connect_error($mysql));
 
 	checkTables($mysql);
 
@@ -12,8 +10,25 @@
 	}
 
 	function addServer($mysql, $host, $port, $user, $pass) {
-		$stmt = $mysql->prepare("INSERT INTO Servers Values(?, ?, ?, ?)");
-		mysqli_stmt_bind_param($stmt, 'siss', $host, $port, $user, $pass);
-		mysqli_stmt_execute($stmt);
+		$connection = ssh2_connect($host, $port);
+		if (!$connection) {
+			return false;
+		} 
+
+		if (!ssh2_auth_password($connection, $user, $pass)) {
+			return false;
+		}
+
+		if (!ssh2_exec($connection, "ls -l")) {
+			return false;
+		}
+		$stmt = $mysql->prepare("INSERT INTO Servers VALUES (?, ?, ?, ?)");
+		$stmt->bind_param('siss', $host, $port, $user, $pass);
+		$stmt->execute();
+		return true;
+	}
+
+	function __destroy() {
+		$mysql->close();
 	}
 ?>
---
+++ stats.php
@@ -0,0 +1,11 @@
+<?php
+	include('includes/functions.php');
+?>
+<html>
+<body>
+	<center>
+		<h2>Server Statistics</h2>
+		<p>This page will show server statistics.</p>
+	</center>
+</body>
+</html>
