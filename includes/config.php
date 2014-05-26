94536c77c734f143c666164139eacc2fb9297b73
TanmayN <nope@nope.nope>
2014-05-25 19:55:56 -0700

    Organized and the functions actually work now

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Welcome to the configuration file.
+// Please enter your database information below.
+
+	$db = array(
+		'username' => "autonode",
+		'password' => "password",
+		'database' => "autonode",
+		'host' => "127.0.0.1"
+	);
+
+?>
